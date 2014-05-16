a96c52e433816d1696229dafa667e7ff7e19f8e4
Zee <Alexander@Alexander-Smiths-MacBook-Pro.local>
2014-05-16 14:51:02 +0100

    added a test class

---
+++ Git Test/GCTest.m
@@ -0,0 +1,13 @@
+//
+//  GCTest.m
+//  Git Test
+//
+//  Created by Alexander Smith on 16/05/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "GCTest.h"
+
+@implementation GCTest
+
+@end
